9f79952a37723a013f4fe3d1225a10ccf3c06534
Charles de Villiers <charles@ska.ac.za>
2016-08-31 12:38:24 +0200

    Installing pylint. Not installing gulp

--- util_installs.sh
+++ util_installs.sh
@@ -1,8 +1,10 @@
-sudo apt-get update
+# Pip installs
+sudo pip install pylint
 sudo pip install ipdb
+# Apt-get installs
+sudo apt-get update
 sudo apt-get install bash-completion
 sudo apt-get install silversearcher-ag
 sudo apt-get install ctags
 sudo apt-get install npm
-sudo npm install -g gulp
 sudo apt-get install multitail
